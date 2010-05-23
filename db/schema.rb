f9178489013c8b564666fc1616dfe7741d2503ba
Leonid Dinershtein <leodg@mail.ru>
2010-05-23 16:06:26 +0400

    added paperclup plugin

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100509084126) do
+ActiveRecord::Schema.define(:version => 20100523120320) do
 
   create_table "categories", :force => true do |t|
     t.integer  "parent_id"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(:version => 20100509084126) do
     t.boolean  "available"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "ingredients", :force => true do |t|
